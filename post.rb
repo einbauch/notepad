ee4fd1eb5b693b5aad13986f60d6a62b57d71518
Obi <alex.kuzminx@gmail.com>
2016-04-26 03:20:42 +0300

    version 1.0

--- post.rb
+++ post.rb
@@ -1,5 +1,13 @@
 class Post
   
+  def self.post_types
+    [Memo, Link, Task]
+  end
+  
+  def self.create(type_index)
+    post_types[type_index].new
+  end
+  
   def initialize
     @created_at = Time.now
     @text = nil
@@ -16,7 +24,7 @@ class Post
   def save
     file = File.new(file_path, 'w:UTF-8')
     
-    for item in strings do
+    for item in to_strings do
       file.puts(item)
     end
     
@@ -26,7 +34,7 @@ class Post
   def file_path
     current_path = File.dirname(__FILE__)
     file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
-    return current_path + '/' + file_name
+    current_path + '/' + file_name
   end
   
 end
